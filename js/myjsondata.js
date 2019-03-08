0077568faa1ce68804d20de2e68eec704fdf3992
Kurniawan Yudha Putrama <kurniawan.yudha.p@gmail.com>
2019-03-08 07:20:27 +0700

    Update json data

--- js/myjsondata.js
+++ js/myjsondata.js
@@ -1,71 +1,58 @@
+// -6.9731756,107.6304271
 var myJsonData = [{
     "id": "1",
-    "longitude": "13.0833",
-    "latitude": "47.75",
-    "description": "This is the description of POI#1",
+    "longitude": "107.6304271",
+    "latitude": "-6.9731756",
+    "description": "Fakultas Ilmu Terapan",
     "altitude": "100.0",
-    "name": "POI#1"
-}, {
+    "name": "FIT"
+}, { //-6.9714539,107.6312274
     "id": "2",
-    "longitude": "13.0833",
-    "latitude": "47.75",
-    "description": "This is the description of POI#2",
+    "longitude": "107.6312274",
+    "latitude": "-6.9714539",
+    "description": "F. Ekonomi Bisnis",
     "altitude": "100.0",
-    "name": "POI#2"
-}, {
+    "name": "FEB"
+}, { // -6.9716589,107.631729
     "id": "3",
-    "longitude": "13.0833",
-    "latitude": "47.75",
-    "description": "This is the description of POI#3",
+    "longitude": "107.631729",
+    "latitude": "-6.9716589",
+    "description": "F. Komunikasi Bisnis",
     "altitude": "100.0",
-    "name": "POI#3"
-}, {
+    "name": "FKB"
+}, { //-6.972596,107.6303369
     "id": "4",
-    "longitude": "13.0833",
-    "latitude": "47.75",
-    "description": "This is the description of POI#4",
+    "longitude": "107.6303369",
+    "latitude": "-6.972596",
+    "description": "F. Industri Kreatif",
     "altitude": "100.0",
-    "name": "POI#4"
-}, {
+    "name": "FIK"
+}, { // -6.9758311,107.6284272
     "id": "5",
-    "longitude": "13.0833",
-    "latitude": "47.75",
-    "description": "This is the description of POI#5",
+    "longitude": "107.6284272",
+    "latitude": "-6.9758311",
+    "description": "Fakultas Informatika",
     "altitude": "100.0",
-    "name": "POI#5"
-}, {
+    "name": "FIF"
+}, { // FRI -6.9757223,107.6290951
     "id": "6",
-    "longitude": "13.0833",
-    "latitude": "47.75",
+    "longitude": "107.6290951",
+    "latitude": "-6.9757223",
     "description": "This is the description of POI#6",
     "altitude": "100.0",
-    "name": "POI#6"
+    "name": "FRI"
 }, {
     "id": "7",
-    "longitude": "13.0833",
-    "latitude": "47.75",
-    "description": "This is the description of POI#7",
+    "longitude": "107.6274035",
+    "latitude": "-6.9755607",
+    "description": "F. Teknik Elektro",
     "altitude": "100.0",
-    "name": "POI#7"
+    "name": "FTE"
 }, {
     "id": "8",
-    "longitude": "13.0833",
-    "latitude": "47.75",
-    "description": "This is the description of POI#8",
+    "longitude": "107.6274473",
+    "latitude": "-6.9729541",
+    "description": "Gedung Kuliah Umum",
     "altitude": "100.0",
-    "name": "POI#8"
-}, {
-    "id": "9",
-    "longitude": "13.0833",
-    "latitude": "47.75",
-    "description": "This is the description of POI#9",
-    "altitude": "100.0",
-    "name": "POI#9"
-}, {
-    "id": "10",
-    "longitude": "13.0833",
-    "latitude": "47.75",
-    "description": "This is the description of POI#10",
-    "altitude": "100.0",
-    "name": "POI#10"
+    "name": "GKU"
 }];
