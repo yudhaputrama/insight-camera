55d7f44250c2e8033b79fd6d5ceb70742c792937
Kurniawan Yudha Putrama <kurniawan.yudha.p@gmail.com>
2019-03-08 08:36:23 +0700

    Remove random noise location

--- js/fromlocalresource.js
+++ js/fromlocalresource.js
@@ -107,14 +107,14 @@ var World = {
     /* Request POI data. */
     requestDataFromLocal: function requestDataFromLocalFn(lat, lon) {
 
-        var poisNearby = Helper.bringPlacesToUser(myJsonData, lat, lon);
-        World.loadPoisFromJsonData(poisNearby);
+        // var poisNearby = Helper.bringPlacesToUser(myJsonData, lat, lon);
+        // World.loadPoisFromJsonData(poisNearby);
 
         /*
             For demo purpose they are relocated randomly around the user using a 'Helper'-function.
             Comment out previous 2 lines and use the following line > instead < to use static values 1:1.
         */
-        /* world.loadPoisFromJsonData(myJsonData);. */
+        world.loadPoisFromJsonData(myJsonData);
     },
 
     onError: function onErrorFn(error) {
